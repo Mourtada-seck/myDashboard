1296eb5f1efc158a7c24a3301e6064e5701a6b33
mourtada-seck <seckmourtada70@gmail.com>
2024-06-01 01:02:54 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("sombre").addEventListener("click", function() {
+    document.getElementById("body").style.background = "#677483";
+});
+
+document.getElementById("clair").addEventListener("click", function() {
+    document.getElementById("body").style.background = "#000";
+});
