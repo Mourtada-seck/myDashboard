b81f7576c64e16510e624bb60aac30e74993a894
mourtada-seck <seckmourtada70@gmail.com>
2024-06-03 02:32:58 +0200

    add sombre

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu_bar');
 const closeBtn = document.querySelector('#close_btn');
-
+const modeSombre = document.querySelector('#sombre');
+const modeClair = document.querySelector('#clair');
 const themeToggler = document.querySelector('.theme-toggler');
 
+// affichage du menu
 menuBtn.addEventListener('click',()=>{
     sideMenu.style.display = "block";
-    menuBtn.style.display = "none";
 });
 
+// fermantation du menu
 closeBtn.addEventListener('click',()=>{
     sideMenu.style.display = "none";
+});
+
+// mode sombre 
+modeSombre.addEventListener("click",()=>{
+    document.body.style.background = "#000"
+})
+
+// mode clair
+modeClair.addEventListener("click",()=>{
+    document.body.style.background = "#fff"
+})
+
+
+themeToggler.addEventListener("click",()=>{
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
 })
